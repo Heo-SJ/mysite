ad8fcdfa245ee5534a7803951b02949724389442
Heo-SJ <gjtjdwo2000@naver.com>
2025-03-31 11:20:39 +0900

    1130

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 # 프레임워크 로드
 from flask import Flask, request, render_template, url_for
 import pandas as pd
-
+import invest
 
 # Flask Class 생성
 app = Flask(__name__)
@@ -40,7 +40,48 @@ def dashboard() :
     # 투자 방식
     input_type = request.args['type']
 
-    return render_template('dashboard.html')
+    # 데이터를 로드
+    df = invest.load_data(input_code, input_time)
+
+    # Class 생성
+    invest_class = invest.Invest(df, _col = 'Close', _start = input_time)
+
+    # input_type을 기준으로 Class의 함수를 호출
+    if input_type == 'bnh' :
+        result = invest_class.buyandhold()
+    elif input_type == 'boll' :
+        result = invest_class.bollinger()
+    elif input_type == 'mmt' :
+        result = invest_class.momentum()
+
+    # 인덱스를 초기화
+    result.reset_index(inplace = True)
+
+    # 특정 컬럼만 필터
+    result = result[ ['Date', 'Close', 'trade', 'rtn', 'acc_rtn'] ]
+
+    # 컬럼의 이름 변경
+    result.columns = ['시간', '종가', '보유내역', '일별 수익률', '누적 수익률']
+
+    # 컬럼들의 이름을 리스트로 생성
+    cols_list = list(result.columns)
+
+    # 테이블에 데이터
+    dict_data = result.to_dict(orient = 'records')
+
+    # x축 데이터
+    x_data = list(result['시간'])
+
+    # y축 데이터
+    y_data = list(result['일별 수익률'])
+    y1_data = list(result['누적 수익률'])
+    
+    return render_template('dashboard.html', 
+                           table_cols = cols_list,
+                           table_data = dict_data,
+                           x_data = x_data,
+                           y_data = y_data,
+                           y1_data = y1_data)
 
 
 
